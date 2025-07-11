38b3dbd40ad6590465ca89fb1c377e6e92c760ee
우다당탕 개발자 <uddt_ds@kakao.com>
2025-07-11 11:31:50 +0900

    MoiveTableViewCell 파일 추가

---
+++ SeSAC7Week3/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  SeSAC7Week3
+//
+//  Created by Lee on 7/11/25.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
