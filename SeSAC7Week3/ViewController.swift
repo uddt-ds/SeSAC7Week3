7b38eabe2260229074b7ccb616432aa687331ef7
우다당탕 개발자 <uddt_ds@kakao.com>
2025-07-11 11:15:47 +0900

    print 내부 숫자 변경

--- SeSAC7Week3/ViewController.swift
+++ SeSAC7Week3/ViewController.swift
@@ -12,11 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(0)
+        print(1)
         print(2)
         print(3)
         print(4)
         print(5)
-        print(6)
     }
 
 
