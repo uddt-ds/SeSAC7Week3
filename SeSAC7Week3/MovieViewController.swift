cdadbc353e26d94bdf2fe9951ee9085f728c7f14
우다당탕 개발자 <uddt_ds@kakao.com>
2025-07-11 12:12:10 +0900

    MovieViewController 파일 생성

---
+++ SeSAC7Week3/MovieViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MovieViewController.swift
+//  SeSAC7Week3
+//
+//  Created by Lee on 7/11/25.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
